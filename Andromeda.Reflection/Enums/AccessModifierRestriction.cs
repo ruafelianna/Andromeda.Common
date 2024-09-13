0ed81c5a619df4524f949dd8c1ea57a5b21b9161
Ruafel <ruafelianna@ya.ru>
2024-09-13 11:50:16 +0300

    Add restriction comparison between two access modifiers

---
+++ Andromeda.Reflection/Enums/AccessModifierRestriction.cs
@@ -0,0 +1,10 @@
+namespace Andromeda.Reflection.Enums
+{
+    public enum AccessModifierRestriction
+    {
+        Error = -1,
+        Equal = 0,
+        LessRestrict = 1,
+        MoreRestrict = 2,
+    }
+}
