22159471f5802986aadf6878d7b0256870e9ee17
Ruafel <ruafelianna@ya.ru>
2024-09-16 14:38:21 +0300

    Add some C# consts; change target framework to netstandard2.0

---
+++ Andromeda.CSharp.Consts/Prefixes.cs
@@ -0,0 +1,9 @@
+using static Andromeda.CSharp.Consts.Keywords;
+
+namespace Andromeda.CSharp.Consts
+{
+    public static class Prefixes
+    {
+        public const string PRE_Global = $"{KW_Global}::";
+    }
+}
